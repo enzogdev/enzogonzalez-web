537a2dfe023cf3b945ad9d1ae4255821744793b3
Enzo Gonzalez <info@enzogonzalez.es>
2024-02-14 14:09:39 +0100

    scroll horizontal, and some changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
       current: 'currentColor',
       transparent: 'transparent',
       white: '#ffffff',
+      black: '#000000',
       primary: colors.stone,
     },
     fontFamily: {
