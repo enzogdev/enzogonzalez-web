2bd461153c33503d046ccc666a5b9c3abaac9543
Enzo Gonzalez <info@enzogonzalez.es>
2024-02-15 13:15:07 +0100

    refactoring imports

---
+++ src/components/ui/index.js
@@ -0,0 +1,5 @@
+export { default as Bento } from './Bento.astro';
+export { default as Container } from './Container.astro';
+export { default as Pill } from './Pill.astro';
+export { default as Avatar } from './Avatar.astro';
+export { default as Button } from './Button.astro';
